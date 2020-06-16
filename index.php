bce3f510182c783a60f1817dec66d7b82094c0bb
tsv-mai <5184488@mail.ru>
2020-06-16 12:54:42 +0300

    New file index.php.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first Document</title>
+</head>
+<body>
+    <? echo "Hello world !!!";
+    ?>
+</body>
+</html>
